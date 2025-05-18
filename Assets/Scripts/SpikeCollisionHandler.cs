1d6e0285a6834dd95860e6ddc813b2cfe21fde14
MatanBelfer <belfer.matan@gmail.com>
2025-05-18 21:05:20 +0300

    fixed spikes not returning to pool

---
+++ Assets/Scripts/SpikeCollisionHandler.cs
@@ -0,0 +1,12 @@
+﻿
+public class Spike : ObjectPoolInterface
+{
+    private void OnBecameInvisible()
+    {
+        // Return to pool 
+        if (objectPoolManager != null && gameObject.activeSelf)
+        {
+            objectPoolManager.InsertToPool(poolName, gameObject);
+        }
+    }
+}
