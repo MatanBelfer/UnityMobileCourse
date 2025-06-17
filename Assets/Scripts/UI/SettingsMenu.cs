723343df8f4dc0dcfa280aa043a2221b5fab6b68
OmriTafla <omritafla@gmail.com>
2025-06-17 12:48:28 +0300

    refactor control scheme and connect to settings menu

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -1,93 +1,102 @@
 using System;
 using TMPro;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SettingsMenu : MonoBehaviour
+namespace RubberClimber
 {
-    //Functionality of the settings menu
-    private float musicVol;
-    private float sfxVol;
-    private ControlScheme controlScheme;
     public enum ControlScheme
     {
         DragAndDrop = 0,
         TapTap = 1 //tap pin to select, tap place to make it move there
     }
-
-    [Header("Sliders")]
-    [SerializeField] private Slider musicVolSlider;
-    [SerializeField] private Slider sfxVolSlider; 
-    [SerializeField] private Slider controlSchemeSlider;
-    [SerializeField] private TMP_Text controlSchemeText;
-
-    private void Start()
-    {
-        musicVolSlider.onValueChanged.AddListener(SetMusicVolume);
-        sfxVolSlider.onValueChanged.AddListener(SetSFXVolume);
-        controlSchemeSlider.onValueChanged.AddListener(SetControlScheme);
-        
-        LoadSettings();
-    }
-
-    private void LoadSettings()
-    {
-        musicVol = PlayerPrefs.GetFloat("musicVol");
-        sfxVol = PlayerPrefs.GetFloat("sfxVol");
-        controlScheme = (ControlScheme)PlayerPrefs.GetInt("controlScheme");
-        //set sliders
-        musicVolSlider.value = musicVol;
-        sfxVolSlider.value = sfxVol;
-        controlSchemeSlider.value = (float)controlScheme;
-        controlSchemeText.text = GetControlSchemeName(controlScheme);
-    }
-
-    public void SetMusicVolume(float volume)
-    {
-        musicVol = volume;
-    }
-    public void SetSFXVolume(float volume)
-    {
-        sfxVol = volume;
-    }
-
-    public void SetControlScheme(float value)
-    {
-        if (value % 1 != 0) Debug.LogError("Control scheme value must be an integer");
-        controlScheme = (ControlScheme)value;
-        controlSchemeText.text = GetControlSchemeName(controlScheme);
-    }
-
-    public string GetControlSchemeName(ControlScheme scheme)
-    {
-        switch (scheme)
-        {
-            case ControlScheme.DragAndDrop:
-                return "Drag and Drop";
-            case ControlScheme.TapTap:
-                return "Tap Tap";
-            default:
-                return "Unknown";
-        }
-    }
-
-    public void CloseMenu()
-    {
-        ApplySettings();
-        SaveSettings();
-        gameObject.SetActive(false);
-    }
     
-    private void ApplySettings()
+    public class SettingsMenu : MonoBehaviour
     {
-        //Apply settings
-    }
+        //Functionality of the settings menu
+        private float musicVol;
+        private float sfxVol;
+        private ControlScheme controlScheme;
+        private InputSystemManager inputSystemManager;
 
-    private void SaveSettings()
-    {
-        PlayerPrefs.SetFloat("musicVol", musicVol);
-        PlayerPrefs.SetFloat("sfxVol", sfxVol);
-        PlayerPrefs.SetInt("controlScheme", (int)controlScheme);
+        [Header("Sliders")]
+        [SerializeField] private Slider musicVolSlider;
+        [SerializeField] private Slider sfxVolSlider; 
+        [SerializeField] private Slider controlSchemeSlider;
+        [SerializeField] private TMP_Text controlSchemeText;
+
+        private void Start()
+        {
+            musicVolSlider.onValueChanged.AddListener(SetMusicVolume);
+            sfxVolSlider.onValueChanged.AddListener(SetSFXVolume);
+            controlSchemeSlider.onValueChanged.AddListener(SetControlScheme);
+            
+            inputSystemManager = InputSystemManager.Instance;
+            
+            LoadSettings();
+        }
+
+        private void LoadSettings()
+        {
+            musicVol = PlayerPrefs.GetFloat("musicVol");
+            sfxVol = PlayerPrefs.GetFloat("sfxVol");
+            controlScheme = (ControlScheme)PlayerPrefs.GetInt("controlScheme");
+            //set sliders
+            musicVolSlider.value = musicVol;
+            sfxVolSlider.value = sfxVol;
+            controlSchemeSlider.value = (float)controlScheme;
+            controlSchemeText.text = GetControlSchemeName(controlScheme);
+        }
+
+        public void SetMusicVolume(float volume)
+        {
+            musicVol = volume;
+        }
+        public void SetSFXVolume(float volume)
+        {
+            sfxVol = volume;
+        }
+
+        public void SetControlScheme(float value)
+        {
+            if (value % 1 != 0) Debug.LogError("Control scheme value must be an integer");
+            controlScheme = (ControlScheme)value;
+            controlSchemeText.text = GetControlSchemeName(controlScheme);
+            inputSystemManager.SetInputMode((ControlScheme)math.round(value));
+        }
+
+        public string GetControlSchemeName(ControlScheme scheme)
+        {
+            switch (scheme)
+            {
+                case ControlScheme.DragAndDrop:
+                    return "Drag and Drop";
+                case ControlScheme.TapTap:
+                    return "Tap Tap";
+                default:
+                    return "Unknown";
+            }
+        }
+
+        public void CloseMenu()
+        {
+            ApplySettings();
+            SaveSettings();
+            gameObject.SetActive(false);
+        }
+        
+        private void ApplySettings()
+        {
+            //Apply settings
+        }
+
+        private void SaveSettings()
+        {
+            PlayerPrefs.SetFloat("musicVol", musicVol);
+            PlayerPrefs.SetFloat("sfxVol", sfxVol);
+            PlayerPrefs.SetInt("controlScheme", (int)controlScheme);
+        }
     }
 }
--- Assets/Scripts/Utils/InputSystemManager.cs
+++ Assets/Scripts/Utils/InputSystemManager.cs
@@ -2,178 +2,191 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class InputSystemManager : MonoBehaviour
+namespace RubberClimber
 {
-    public PlayerInputActions InputActions;
-
-    public static InputSystemManager Instance { get; private set; }
-
-    private bool InputMode = false; //false = drag, true = select
-    
-    [Header("Drag Mode Settings")] [SerializeField]
-    private float maxClickDistance = 0.5f; // Max distance to detect pin clicks
-
-    private PinLogic CurrentPin = null;
-
-    private void Awake()
+    public class InputSystemManager : MonoBehaviour
     {
-        if (Instance == null)
+        public PlayerInputActions InputActions;
+
+        public static InputSystemManager Instance { get; private set; }
+
+        private ControlScheme InputMode = ControlScheme.DragAndDrop;
+
+        [Header("Drag Mode Settings")] [SerializeField]
+        private float maxClickDistance = 0.5f; // Max distance to detect pin clicks
+
+        private PinLogic CurrentPin = null;
+
+        private void Awake()
         {
-            Instance = this;
-            InputActions = new PlayerInputActions();
+            if (Instance == null)
+            {
+                Instance = this;
+                InputActions = new PlayerInputActions();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
         }
-        else
+
+        private void OnEnable()
         {
-            Destroy(gameObject);
+            InputActions.PinMovement.Enable();
+            InputActions.PinMovement.Click.started += OnClickStarted;
+            InputActions.PinMovement.Click.canceled += OnClickEnded;
+            InputActions.PinMovement.Position.performed += OnPositionChanged;
+            InputActions.PinMovement.ToggleMode.started += OnToggleModePressed;
         }
-    }
 
-    private void OnEnable()
-    {
-        InputActions.PinMovement.Enable();
-        InputActions.PinMovement.Click.started += OnClickStarted;
-        InputActions.PinMovement.Click.canceled += OnClickEnded;
-        InputActions.PinMovement.Position.performed += OnPositionChanged;
-        InputActions.PinMovement.ToggleMode.started += OnToggleModePressed;
-    }
-
-    private void OnDisable()
-    {
-        InputActions.PinMovement.Click.started -= OnClickStarted;
-        InputActions.PinMovement.Click.canceled -= OnClickEnded;
-        InputActions.PinMovement.Position.performed -= OnPositionChanged;
-        InputActions.PinMovement.ToggleMode.started -= OnToggleModePressed;
-        InputActions.PinMovement.Disable();
-    }
-
-    private void OnDestroy()
-    {
-        InputActions.Dispose();
-    }
-
-    private void OnClickStarted(InputAction.CallbackContext context)
-    {
-        Vector3 clickPosition = ScreenToWorldPosition(InputActions.PinMovement.Position.ReadValue<Vector2>());
-
-        PinLogic tmpPin = FindClosestPin(clickPosition);
-        if (tmpPin != null)
+        private void OnDisable()
         {
-            if (InputMode)
+            InputActions.PinMovement.Click.started -= OnClickStarted;
+            InputActions.PinMovement.Click.canceled -= OnClickEnded;
+            InputActions.PinMovement.Position.performed -= OnPositionChanged;
+            InputActions.PinMovement.ToggleMode.started -= OnToggleModePressed;
+            InputActions.PinMovement.Disable();
+        }
+
+        private void OnDestroy()
+        {
+            InputActions.Dispose();
+        }
+
+        private void OnClickStarted(InputAction.CallbackContext context)
+        {
+            Vector3 clickPosition = ScreenToWorldPosition(InputActions.PinMovement.Position.ReadValue<Vector2>());
+
+            PinLogic tmpPin = FindClosestPin(clickPosition);
+            if (tmpPin != null)
+            {
+                if (InputMode == ControlScheme.TapTap)
+                {
+                    //select mode
+                }
+                else
+                {
+                    //drag mode
+                    CurrentPin = tmpPin;
+                    Debug.Log("drag mode clicked");
+                    CurrentPin.StartFollowingPin(CurrentPin);
+                }
+            }
+            else
+            {
+                Debug.Log("Clicked on nothing");
+            }
+        }
+
+        private void OnClickEnded(InputAction.CallbackContext context)
+        {
+            Vector3 endPosition = ScreenToWorldPosition(InputActions.PinMovement.Position.ReadValue<Vector2>());
+            if (InputMode == ControlScheme.TapTap)
             {
                 //select mode
+                PinLogic tmpPin = FindClosestPin(endPosition);
+                if (tmpPin == null && CurrentPin != null)
+                {
+                    CurrentPin.MovePinToPosition(CurrentPin, endPosition);
+                    CurrentPin.StopFollowingPin(CurrentPin);
+                    CurrentPin = null;
+                }
+                else
+                {
+                    if (CurrentPin != null && CurrentPin == tmpPin)
+                    {
+                        CurrentPin = null; //reset selection
+                    }
+                    else
+                    {
+                        CurrentPin = tmpPin; // selected another pin 
+                    }
+                }
             }
             else
             {
                 //drag mode
-                CurrentPin = tmpPin;
-                Debug.Log("drag mode clicked");
-                CurrentPin.StartFollowingPin(CurrentPin);
-            }
-        }
-        else
-        {
-            Debug.Log("Clicked on nothing");
-        }
-    }
 
-    private void OnClickEnded(InputAction.CallbackContext context)
-    {
-        Vector3 endPosition = ScreenToWorldPosition(InputActions.PinMovement.Position.ReadValue<Vector2>());
-        if (InputMode)
-        {
-            //select mode
-            PinLogic tmpPin = FindClosestPin(endPosition);
-            if (tmpPin == null && CurrentPin != null)
-            {
+
                 CurrentPin.MovePinToPosition(CurrentPin, endPosition);
                 CurrentPin.StopFollowingPin(CurrentPin);
                 CurrentPin = null;
             }
-            else
+        }
+
+
+        private void OnPositionChanged(InputAction.CallbackContext context)
+        {
+            if (CurrentPin != null && CurrentPin.isFollowing &&
+                InputMode == ControlScheme.DragAndDrop) // Only in drag mode
             {
-                if (CurrentPin != null && CurrentPin == tmpPin)
+                Vector2 screenPosition = context.ReadValue<Vector2>();
+                Vector3 worldPosition = ScreenToWorldPosition(screenPosition);
+                CurrentPin.transform.position = worldPosition;
+            }
+        }
+
+        private void OnToggleModePressed(InputAction.CallbackContext context)
+        {
+            SetInputMode(InputMode == ControlScheme.DragAndDrop
+                ? ControlScheme.TapTap
+                : ControlScheme.DragAndDrop);
+        }
+
+        public void SetInputMode(ControlScheme newMode)
+        {
+            if (newMode == InputMode) return;
+
+            InputMode = newMode;
+
+            // Cancel any current dragging when switching modes
+            if (CurrentPin != null)
+            {
+                if (CurrentPin.isFollowing)
                 {
-                    CurrentPin = null; //reset selection
+                    CurrentPin.StopFollowingPin(CurrentPin);
                 }
-                else
+
+                CurrentPin = null;
+            }
+
+            Debug.Log($"Switched to {InputMode}");
+        }
+
+        private Vector3 ScreenToWorldPosition(Vector2 screenPosition)
+        {
+            if (Camera.main == null)
+            {
+                Debug.LogWarning("No main camera found!");
+                return Vector3.zero;
+            }
+
+            Vector3 worldPosition =
+                Camera.main.ScreenToWorldPoint(new Vector3(screenPosition.x, screenPosition.y,
+                    Camera.main.nearClipPlane));
+            worldPosition.z = 0f;
+            return worldPosition;
+        }
+
+        private PinLogic FindClosestPin(Vector3 worldPosition)
+        {
+            PinLogic[] allPins = FindObjectsByType<PinLogic>(FindObjectsSortMode.None);
+            PinLogic closestPin = null;
+            float closestDistance = float.MaxValue;
+
+            foreach (PinLogic pin in allPins)
+            {
+                if (pin == null) continue;
+
+                float distance = Vector3.Distance(pin.transform.position, worldPosition);
+                if (distance < closestDistance && distance <= maxClickDistance)
                 {
-                    CurrentPin = tmpPin; // selected another pin 
+                    closestDistance = distance;
+                    closestPin = pin;
                 }
             }
+
+            return closestPin;
         }
-        else
-        {
-            //drag mode
-
-
-            CurrentPin.MovePinToPosition(CurrentPin, endPosition);
-            CurrentPin.StopFollowingPin(CurrentPin);
-            CurrentPin = null;
-        }
-    }
-
-
-    private void OnPositionChanged(InputAction.CallbackContext context)
-    {
-        if (CurrentPin != null && CurrentPin.isFollowing && !InputMode) // Only in drag mode
-        {
-            Vector2 screenPosition = context.ReadValue<Vector2>();
-            Vector3 worldPosition = ScreenToWorldPosition(screenPosition);
-            CurrentPin.transform.position = worldPosition;
-        }
-    }
-
-    private void OnToggleModePressed(InputAction.CallbackContext context)
-    {
-        InputMode = !InputMode;
-
-        // Cancel any current dragging when switching modes
-        if (CurrentPin != null)
-        {
-            if (CurrentPin.isFollowing)
-            {
-                CurrentPin.StopFollowingPin(CurrentPin);
-            }
-
-            CurrentPin = null;
-        }
-
-        string modeText = InputMode ? "SELECT MODE" : "DRAG MODE";
-        Debug.Log($"Switched to {modeText}");
-    }
-
-    private Vector3 ScreenToWorldPosition(Vector2 screenPosition)
-    {
-        if (Camera.main == null)
-        {
-            Debug.LogWarning("No main camera found!");
-            return Vector3.zero;
-        }
-
-        Vector3 worldPosition =
-            Camera.main.ScreenToWorldPoint(new Vector3(screenPosition.x, screenPosition.y, Camera.main.nearClipPlane));
-        worldPosition.z = 0f;
-        return worldPosition;
-    }
-
-    private PinLogic FindClosestPin(Vector3 worldPosition)
-    {
-        PinLogic[] allPins = FindObjectsByType<PinLogic>(FindObjectsSortMode.None);
-        PinLogic closestPin = null;
-        float closestDistance = float.MaxValue;
-
-        foreach (PinLogic pin in allPins)
-        {
-            if (pin == null) continue;
-
-            float distance = Vector3.Distance(pin.transform.position, worldPosition);
-            if (distance < closestDistance && distance <= maxClickDistance)
-            {
-                closestDistance = distance;
-                closestPin = pin;
-            }
-        }
-
-        return closestPin;
     }
 }
