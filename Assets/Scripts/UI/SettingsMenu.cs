8e513a83a2a99ba74137103348a6ad824c822f88
OmriTafla <omritafla@gmail.com>
2025-06-22 20:54:07 +0300

    merge

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -1,41 +1,18 @@
 using System;
-using TMPro;
-using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
-using System.Collections.Generic;
 
 public class SettingsMenu : MonoBehaviour
 {
-    //Functionality of the settings menu
-    // private float musicVol;
-    // private float masterVol;
-    // private ControlScheme controlScheme;
-    private InputSystemManager inputSystemManager;
-
     private SettingsItem[] settingsItems;
-    
-    // [Header("Sliders")]
-    // [SerializeField] private Slider musicVolSlider;
-    // [SerializeField] private Slider masterVolSlider; 
-    // [SerializeField] private Slider controlSchemeSlider;
-    // [SerializeField] private TMP_Text controlSchemeText;
     
     [Header("MainMenu")]
     [SerializeField] private MainMenuCallbacks mainMenu;
 
     private void Start()
     {
-        // musicVolSlider.onValueChanged.AddListener(SetMusicVolume);
-        // masterVolSlider.onValueChanged.AddListener(SetSFXVolume);
-        // controlSchemeSlider.onValueChanged.AddListener(SetControlScheme);
         settingsItems = GetComponentsInChildren<SettingsItem>();
         
-        inputSystemManager = InputSystemManager.Instance;
-        
         LoadSettings();
-        // SetAllSettings();
         
         if (mainMenu != null) mainMenu.OnStartGame += CloseMenu;
     }
@@ -63,53 +40,8 @@ public class SettingsMenu : MonoBehaviour
                 item.SetValueBool(PlayerPrefs.GetString(prefsName) == "True");
             }
         }
-        
-        // musicVol = PlayerPrefs.GetFloat("musicVol");
-        // masterVol = PlayerPrefs.GetFloat("masterVol");
-        // controlScheme = (ControlScheme)PlayerPrefs.GetInt("controlScheme");
-        //set sliders and toggles to the saved values
-        
-        // musicVolSlider.value = musicVol;
-        // masterVolSlider.value = masterVol;
-        // controlSchemeSlider.value = (float)controlScheme;
-        // controlSchemeText.text = controlScheme.ToString();
     }
-
-    // private void SetAllSettings()
-    // {
-    //     // SetMusicVolume(musicVol);
-    //     // SetSFXVolume(masterVol);
-    //     // SetControlScheme(controlScheme);
-    // }
-
-    // public void SetMusicVolume(float volume)
-    // {
-    //     musicVol = volume;
-    // }
-    //
-    // public void SetSFXVolume(float volume)
-    // {
-    //     masterVol = volume;
-    // }
-
-    // public void SetControlScheme(float value)
-    // {
-    //     if (value % 1 != 0) Debug.LogError("Control scheme value must be an integer");
-    //     controlScheme = (ControlScheme)value;
-    //     SetControlScheme(controlScheme);
-    // }
-
-    // private void SetControlScheme(ControlScheme scheme)
-    // {
-    //     // controlSchemeText.text = controlScheme.ToString();
-    //     inputSystemManager?.SetControlScheme(scheme);
-    // }
-
-    // public void SetDifficulty(float value)
-    // {
-    //     
-    // }
-
+    
     public void CloseMenu()
     {
         SaveSettings();
@@ -139,10 +71,5 @@ public class SettingsMenu : MonoBehaviour
                 PlayerPrefs.SetString(prefsName, value ? "True" : "False");
             }
         }
-        
-        // PlayerPrefs.SetFloat("musicVol", musicVol);
-        // PlayerPrefs.SetFloat("masterVol", masterVol);
-        // PlayerPrefs.SetInt("controlScheme", (int)controlScheme);
-        //print("Settings Saved");
     }
 }
